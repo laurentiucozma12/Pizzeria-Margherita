a70e0c29910435df3cc8ac4150391dc4d09936f3
Lau <laurentiucozma12@gmail.com>
2023-05-28 18:20:32 +0300

    examples_routing_&_api

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +18,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return response('Hello World');
-});
+// Referecen, examples:
+// Route::get('/hello', function () {
+//     return response('<h1>Hello World</h1>', 200)
+//         ->header('Content-Type', 'text/plain')
+//         ->header('foo', 'bar');
+// });
  
+// Route::Get('/posts/{id}', function($id) {
+//     return response('POST ' . $id);
+// })->where('id', '[0-9]+');
+
+// Route::get('/search', function(Request $request) {
+//     return ($request->name . ' ' . $request->city);
+// });
